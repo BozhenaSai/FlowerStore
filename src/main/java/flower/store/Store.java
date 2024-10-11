fea450ddca7dada04719c1c0ef2f2a4ca1916fba
BozhenaSai <149332573+BozhenaSai@users.noreply.github.com>
2024-10-11 22:52:00 +0300

    implemented classes

---
+++ src/main/java/flower/store/Store.java
@@ -0,0 +1,30 @@
+package flower.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+    private List<FlowerBucket> flowerBuckets;
+
+    public Store() {
+        this.flowerBuckets = new ArrayList<>();
+    }
+
+    public void addFlowerBucket(FlowerBucket bucket) {
+        flowerBuckets.add(bucket);
+    }
+
+    public List<FlowerPack> searchByType(FlowerType flowerType) {
+        List<FlowerPack> foundFlowerPacks = new ArrayList<>();
+        
+        for (FlowerBucket bucket : flowerBuckets) {
+            for (FlowerPack pack : bucket.getFlowerPacks()) {
+                if (pack.getFlower().getFlowerType().equals(flowerType)) {
+                    foundFlowerPacks.add(pack);
+                }
+            }
+        }
+
+        return foundFlowerPacks;
+    }
+}
