f0fb82f505f947acdb4abd326d810cc8717d84a7
BozhenaSai <149332573+BozhenaSai@users.noreply.github.com>
2024-10-11 22:49:18 +0300

    implemented classes

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,21 +1,50 @@
 package flower.store;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
-
-
+@Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Flower {
-    @Getter
     private double sepalLength;
     private FlowerColor color;
-    @Getter
     private double price;
-    @Getter
     private FlowerType flowerType;
+
+    public Flower(double sepalLength, FlowerColor color, double price) {
+        this.sepalLength = sepalLength;
+        this.color = color;
+        this.price = price;
+    }
+
+    public double setSepalLength(double sepalLength) {
+        return this.sepalLength = sepalLength;
+    }
+    
+    public double getSepalLength() {
+        return sepalLength;
+    }
+
+    public double setPrice(double price) {
+        return this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setColor(FlowerColor color) {
+        this.color = color;
+    }
 
     public String getColor() {
         return color.toString();
     }
+
 }
